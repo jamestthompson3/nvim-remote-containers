20d6d8073a9ff12a5a298e1100830635e2578385
Christian Decker <christian@cdecker.io>
2022-02-13 12:38:09 -0500

    Moved helper functions from docker to utils. Made docker compose errors appear

--- lua/utils.lua
+++ lua/utils.lua
@@ -1,29 +1,91 @@
 local M = {}
-local loop = vim.loop
+local uv = vim.loop
+local message = {}
 
-local function safe_close(handle)
-	if not loop.is_closing(handle) then
-		loop.close(handle)
+function M.systemChecks()
+	if not (vim.fn.executable("docker")) then
+		error("must install docker for this functionality")
+	end
+	if not (M.exists(".devcontainer")) then
+		error("Could not find .devcontainer folder")
+	end
+	if not (M.exists(".devcontainer/devcontainer.json")) then
+		error("Could not find .devcontainer/devcontainer.json")
 	end
 end
 
+function M.parseConfig(configType)
+	M.systemChecks()
+
+	-- TODO: Check current directory and the devcontainer
+	local parsedConfig = vim.fn.json_decode(vim.fn.join(vim.fn.readfile(".devcontainer/devcontainer.json")))
+	if not parsedConfig.image and not parsedConfig.dockerComposeFile and not parsedConfig.dockerFile then
+		error("must have an image, dockerfile, or docker-compose file")
+		return
+	end
+
+	if not parsedConfig[configType] then
+		error("Must have " .. configType .. " defined in the devcontainer.json")
+		return
+	end
+	return parsedConfig
+end
+
+local function safe_close(handle)
+	if not uv.is_closing(handle) then
+		uv.close(handle)
+	end
+end
+
+local function fmt(data)
+	local vars = vim.split(data, "\n")
+	for _, d in pairs(vars) do
+		table.insert(message, d)
+	end
+end
+
+local function onread(err, data)
+	if err then
+		fmt(err)
+	end
+	if data then
+		fmt(data)
+	end
+end
+
+local function showError()
+	local buf = M.floatingWindow()
+	vim.api.nvim_buf_set_lines(buf, 0, -1, true, message)
+	vim.api.nvim_buf_set_option(buf, "modifiable", false)
+end
+
 function M.spawn(cmd, opts, onexit)
-	local inpt = { stdout = opts.stdout or function() end, stderr = opts.stderr or function() end }
+	local inpt = { stdout = opts.stdout or function() end, stderr = onread or opts.stderr or function() end }
 	local handle
-	local stdout = loop.new_pipe(false)
-	local stderr = loop.new_pipe(false)
-	handle, _ = loop.spawn(cmd, vim.tbl_extend("force", opts, { stdio = { stdout, stderr } }), function(code, signal)
-		if type(onexit) == "function" then
-			onexit(code, signal)
-		end
-		loop.read_stop(stdout)
-		loop.read_stop(stderr)
-		safe_close(handle)
-		safe_close(stdout)
-		safe_close(stderr)
-	end)
-	loop.read_start(stdout, inpt.stdout)
-	loop.read_start(stderr, inpt.stderr)
+	local stdout = uv.new_pipe(false)
+	local stderr = uv.new_pipe(false)
+
+	handle, _ = uv.spawn(
+		cmd,
+		vim.tbl_extend("force", opts, { stdio = { nil, stdout, stderr } }),
+		vim.schedule_wrap(function(code, signal)
+			uv.read_stop(stdout)
+			uv.read_stop(stderr)
+			safe_close(handle)
+			safe_close(stdout)
+			safe_close(stderr)
+
+			print(code, signal)
+			if code == 1 then
+				showError()
+			end
+			if type(onexit) == "function" then
+				onexit()
+			end
+		end)
+	)
+	uv.read_start(stdout, inpt.stdout)
+	uv.read_start(stderr, inpt.stderr)
 end
 
 --- Check if a file or directory exists in this path
@@ -79,6 +141,7 @@ function M.floatingWindow()
 
 		-- create a new floating window, centered in the editor
 		vim.api.nvim_open_win(buf, true, opts)
+		return buf
 	end
 end
 
